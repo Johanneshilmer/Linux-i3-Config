e47d9dc4a4c1cb05f0eeccc17f7e2e922240e160
Johannes Hilmerson <115149741+Johanneshilmer@users.noreply.github.com>
2024-06-19 19:17:55 +0200

    Create xorg.conf.d
    asd

---
+++ xorg.conf.d
@@ -0,0 +1,14 @@
+If you user touchpad and want to change to soft touch and also "Natural scrolling".
+
+In a folder named "xorg.conf.d", you create a file called "90-touchpad.conf".
+Inside this file you insert.
+
+Section "InputClass"
+        Identifier "touchpad"
+        MatchIsTouchpad "on"
+        Driver "libinput"
+        Option "Tapping" "on"
+	Option "NaturalScrolling" "on"
+	Option "ScrollMethod" "twofinger"
+	Option "TappingButtonMap" "lrm"
+EndSection
