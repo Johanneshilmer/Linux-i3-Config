b108ad3671d8d6b8be4234352f2b92080da22744
Johannes Hilmerson <115149741+Johanneshilmer@users.noreply.github.com>
2024-06-19 19:20:52 +0200

    Update xorg.conf.d
    update

--- xorg.conf.d
+++ xorg.conf.d
@@ -3,6 +3,9 @@ If you user touchpad and want to change to soft touch and also "Natural scrollin
 In a folder named "xorg.conf.d", you create a file called "90-touchpad.conf".
 Inside this file you insert.
 
+cd in to : cd /etc/X11/xorg.conf.d
+and create the file.
+
 Section "InputClass"
         Identifier "touchpad"
         MatchIsTouchpad "on"
